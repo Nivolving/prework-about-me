88c764dfb93af768d6d1a20d744da43e301bda76
Nivolving <nivethapartheeban@gmail.com>
2020-03-23 19:24:35 -0700

    updated JS files

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+        document.getElementById("growBtn").addEventListener("click", function() {
+
+            document.getElementById("box").style.height = "250px";
+        });
+        document.getElementById("resetBtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+        });
+        document.getElementById("bluebtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.backgroundColor="blue";
+        });
+        document.getElementById("opacitybtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.opacity="0.5";
+        });
+       
+       
