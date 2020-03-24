2c46863001b6d92028aff37ea1867ce9576c0203
Nivolving <nivethapartheeban@gmail.com>
2020-03-23 20:01:22 -0700

    updated JS files

--- javascript.js
+++ javascript.js
@@ -6,6 +6,9 @@
         document.getElementById("resetBtn").addEventListener("click", function(){
 
             document.getElementById("box").style.height = "150px";
+            document.getElementById("box").style.backgroundColor="orange";
+            document.getElementById("box").style.opacity="1";
+
         });
         document.getElementById("bluebtn").addEventListener("click", function(){
 
